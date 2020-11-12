d385b27bd9dbbe2bad1061bfc4e09a282e9341e8
Mateus Nunes <mateusnss@protonmail.ch>
2020-11-12 19:06:01 -0300

    Create Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+//Este programa imprime "Olá, mundo!" no console e caso receba algum argumento para a classe principal, imprime o mesmo.
+
+namespace HelloWorl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tOlá, mundo!");
+            if(args.GetLength(0) > 0)
+            {
+                Console.WriteLine(args.GetValue(0));
+            }
+        }
+    }
+}
