8d775320bccd10b9fe7f2a1968ab668f68142f2a
Mateus Nunes <mateusnss@protonmail.ch>
2021-01-16 15:18:54 -0300

    16-01-2021

---
+++ temp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace temp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string number = "56738457134614591365734659165734593478561238756178356413658956738571461459136573465916573459";
+
+            BigInteger n = BigInteger.Parse(number);
+
+            int i = 0;
+            while(i < 100){
+                n *= n;
+                i++;
+                Console.Write($"{i} ... ");
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ teste_de_primalidade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace teste_de_primalidade
+{
+    class Program
+    {
+        static bool fun(double n){
+            double lim = Math.Sqrt(n);
+            Console.WriteLine(lim);
+            if(n > 1){
+                for(double i = 2; i <= lim; i++){
+                    if(n % i == 0){
+                        return false;
+                    }
+                }return true;
+            }return false;
+        }
+
+        static void Main(string[] args)
+        {
+            double n = double.Parse(args[0]);
+            Console.WriteLine(fun(n));
+        }
+    }
+}
